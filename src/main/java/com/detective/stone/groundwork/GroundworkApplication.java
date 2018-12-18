52dc1a52586ad5cbc2388155b974a50f3df5807a
detective <13750846673@163.com>
2018-12-18 20:59:47 +0800

    first commit

---
+++ src/main/java/com/detective/stone/groundwork/GroundworkApplication.java
@@ -0,0 +1,14 @@
+package com.detective.stone.groundwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroundworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroundworkApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/detective/stone/groundwork/GroundworkApplicationTests.java
@@ -0,0 +1,17 @@
+package com.detective.stone.groundwork;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GroundworkApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
